c4d24667dab6b275903fbecd0faaf696f9d5f27e
rutuja091 <gaikwadrutuja254@gmail.com>
2024-09-02 14:30:47 +0530

    add router

---
+++ src/views/Add/Add.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Add.css"
+function Add() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Add
---
+++ src/views/Show/Show.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Show.css"
+function Show() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Show
